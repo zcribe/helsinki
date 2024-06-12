ca3f11032d0e0d25b878dd26f34b30925d99cdf4
Erlend <erlend.eelmets@gmail.com>
2024-06-12 19:03:51 +0300

    1.1, 1.2, 1.3, 1.4, 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,63 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const Header = ({course}) => {
+    return <h1>{course.name}</h1>;
+  };
+
+  const Part = ({ part, exercises }) => {
+    return (
+      <p>
+        {part} {exercises}
+      </p>
+    );
+  };
+
+  const Content = ({ part1, part2, part3 }) => {
+    return (
+      <>
+        <Part part={part1.name} exercises={part1.exercises} />
+        <Part part={part2.name} exercises={part1.exercises} />
+        <Part part={part3.name} exercises={part1.exercises} />
+      </>
+    );
+  };
+
+  const Total = ({ exercises1, exercises2, exercises3 }) => {
+    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+  };
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={course.parts[0]}
+        part2={course.parts[1]}
+        part3={course.parts[2]}
+      />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
